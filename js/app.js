8443f5d577efa4441e4d14ffc47e7d40f8450d0a
unknown <ortiztato@gmail.com>
2022-07-25 09:23:20 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+const contactbtn = document.querySelector("#contactbtn");
+const galeriabtn = document.querySelector("#galeriabtn");
+const cateringbtn = document.querySelector("#cateringbtn");
+
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+contactbtn.addEventListener("click", toggleMenu);
+galeriabtn.addEventListener("click", toggleMenu);
+cateringbtn.addEventListener("click", toggleMenu);
+
