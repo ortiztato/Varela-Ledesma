b8c3f7565aabd39a7f40dcbbe3060348b31b32ea
unknown <ortiztato@gmail.com>
2022-08-15 13:10:33 -0600

    modificado portada y gifs

--- js/scroll.js
+++ js/scroll.js
@@ -10,13 +10,12 @@ window.onscroll = () => {
         const estudiobtn = document.querySelector("#estudiobtn");
         const guiasbtn = document.querySelector("#guiasbtn");
         const logo = document.querySelector(".logotype");
-        const logosonscroll = document.querySelector(".logotypeonscroll");
         const logotype = document.querySelector(".logotype");
 
 
         if (window.scrollY >= window.innerHeight - 100) {
 
-            document.querySelector(".topheader").style.background = '#1d1d1d';
+
             contactbtn.style.color = '#f5f5f5';
             sociosbtn.style.color = '#f5f5f5';
             areasbtn.style.color = '#f5f5f5';
@@ -24,14 +23,13 @@ window.onscroll = () => {
             guiasbtn.style.color = '#f5f5f5';
             logo.style.color = '#f5f5f5';
             burgerbtn.fill = '#f5f5f5';
-            logosonscroll.style.cssText = "display: inline";
             logotype.style.cssText = "display: none";
         }
 
 
         else {
 
-            document.querySelector(".topheader").style.background = '#00000000';
+
             contactbtn.style.color = '#1d1d1d';
             sociosbtn.style.color = '#1d1d1d';
             areasbtn.style.color = '#1d1d1d';
@@ -39,9 +37,23 @@ window.onscroll = () => {
             guiasbtn.style.color = '#1d1d1d';
             logo.style.color = '#1d1d1d';
             burgerbtn.fill = '#1d1d1d';
-            logosonscroll.style.cssText = "display: none";
             logotype.style.cssText = "display: none";
         }
+
+    }
+    if (window.scrollY >= window.innerHeight - 100) {
+        document.querySelector(".topheader").style.background = '#1d1d1d';
+        document.querySelector(".logotypescrollwhite").style.cssText = "display: inline";
+        document.querySelector(".logotypescrollblack").style.cssText = "display: none";
+        document.querySelector("#burgerbtnwhite").style.cssText = "display: inline";
+        document.querySelector("#burgerbtn").style.cssText = "display: none";
+    }
+    else {
+        document.querySelector(".topheader").style.background = '#00000000';
+        document.querySelector(".logotypescrollwhite").style.cssText = "display: none";
+        document.querySelector(".logotypescrollblack").style.cssText = "display: inline";
+        document.querySelector("#burgerbtnwhite").style.cssText = "display: none";
+        document.querySelector("#burgerbtn").style.cssText = "display: inline";
 
     }
 
